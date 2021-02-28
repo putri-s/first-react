e7d533a0d74d6b43a6cc67c822d577ca8a85906f
putri-s <77227382+putri-s@users.noreply.github.com>
2021-02-28 22:19:17 +0700

    conditional rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = ({ showAddToggle, showAdd }) => {
   return (
     //me-return isi komponen header (section header)
     <section className="header">
-      <button className="header-btn black-color">Add</button>
+      <button className="header-btn black-color" onClick={showAddToggle}>
+        {showAdd ? `Finish` : `Add`}
+      </button>
       <h1 className="header-title">Todo Lists</h1>
       <button className="header-btn red-color">Clear</button>
     </section>
   );
+};
+
+Header.propTypes = {
+  showAddToggle: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
 };
 
 export default Header;
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -13,17 +13,22 @@ const TodoList = () => {
     { text: `Belajar3` }
   ]);
 
+  //state showAdd
+  const [showAdd, setShowAdd] = useState(false);
+
   const addTodo = (value) => {
     const addedTodo = [...todos, { text: value }];
 
     setTodos(addedTodo);
   };
 
+  const showAddToggle = () => setShowAdd(!showAdd);
+
   return (
     <Paper>
-      <Header />
+      <Header showAddToggle={showAddToggle} showAdd={showAdd} />
 
-      <TodoForm addTodo={addTodo} />
+      <TodoForm addTodo={addTodo} showAdd={showAdd} />
 
       <Todos todos={todos} />
     </Paper>
